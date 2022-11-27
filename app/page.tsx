0012e20b85e28f51941fb4a9b7b176d4f88e5d58
nia-h <nia.hq@yahoo.com>
2022-11-27 09:12:29 -0500

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import styles from './page.module.css';
 import Image from 'next/image';
-import archPic from '../public/arch.ico'
-import SignupModal from './SignupModal'
-import LoginModal from './LoginModal'
+import archPic from '.././public/arch.ico';
+import SignupModal from './SignupModal';
+import LoginModal from './LoginModal';
 
 //home page component
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>National Parks Digital Passport</h1>
-      <Image src={archPic} alt="icon showing Delicate Arch in Moab" width={250} height={250}/>
+      <Image src={archPic} alt='icon showing Delicate Arch in Moab' width={250} height={250} />
       <SignupModal />
-      <br/>
+      <br />
       <LoginModal />
     </div>
-  )
+  );
 }
