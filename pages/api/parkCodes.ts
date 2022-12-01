c07625747b58bce69058823e42c252671018bef0
nia-h <nia.hq@yahoo.com>
2022-11-30 23:17:39 -0500

    replaced local prisma client with global prisma client in parkCodes.ts

--- pages/api/parkCodes.ts
+++ pages/api/parkCodes.ts
@@ -1,28 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Park } from '../../typings';
-import { PrismaClient } from '@prisma/client';
+
+import prisma from '../../lib/prisma';
 
 type Data = {
-  name: {}
+  name: {};
 };
-
-
-const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const parks = await prisma.park.findMany({
     select: {
-      park_code: true
+      park_code: true,
     },
   });
   prisma.$disconnect();
 
   //const data: string = JSON.stringify(parks);
   const parkCodes = parks.map(el => {
-    return el.park_code
-  })
+    return el.park_code;
+  });
 
   //console.log(parkCodes)
 
-  res.status(200).json({name: parkCodes});
+  res.status(200).json({ name: parkCodes });
 }
